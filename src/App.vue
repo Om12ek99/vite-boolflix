c9ac22377372ff1c53d6ffd286c68c07c05ddfc4
Om12ek99 <omar.elkamili@gmail.com>
2024-05-09 23:26:52 +0200

    add: constant dbValues for database navigation

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,17 @@ export default{
     return {
       store,
     }
+  },
+  methods: {
+  sortingMedia(){
+    const dbValues = {                    //dbValues sono i valori costanti che mi servono per accedere e navigare il database
+      api_key: this.store.apiKey,         //richiama la chiave di accesso
+      query: this.store.searchQuery,      //richiama la stringa con il valore che vogliamo cercare
+    }
+    axios.get("https://api.themoviedb.org/3/search/movie",)
+
   }
+}
 }
 
 </script>
