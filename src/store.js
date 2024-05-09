59b62d948f5faa27e97d262c6dbe4e48fc24cb8a
Om12ek99 <omar.elkamili@gmail.com>
2024-05-09 23:21:12 +0200

    add:store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
-import { reactive } from 'vue';
-export const store = reactive ({
-    apiKey:4875e1f3e6e0b3d38e3d893c57795332,
-    
-  })
+
+import { reactive } from "vue";
+export const store = reactive({
+    arrayFilm: [],         //array dei film vuoto
+    arraySerie: [],       //array delle serie tv vuoto
+    searchQuery: "ciao", // stringa di testo che miserve per cercare un valore
+    apiKey: "4875e1f3e6e0b3d38e3d893c57795332",   //key per accedere al database
+})
